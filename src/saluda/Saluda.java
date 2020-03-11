3ab43705a76ac1c1096613614b01a4f94242a5d7
Sergio <Sergio@PORTATIL-SERGIO>
2020-03-11 13:48:52 +0100

    commit

---
+++ src/saluda/Saluda.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package saluda;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Sergio
+ */
+public class Saluda {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String nombre;
+
+        System.out.print("Introduzca su nombre: ");
+        nombre = sc.nextLine();
+
+        System.out.println("Hola " + nombre);
+        
+    }
+
+}
